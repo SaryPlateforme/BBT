40b24c449af78556831d6ce9a7dcc568c16e7d8b
Kenzo douchet <kenzodouchet33@gmail.com>
2025-07-03 10:13:47 +0200

    modification hamburger

--- nav.js
+++ nav.js
@@ -1,6 +1,14 @@
 const hamburger = document.querySelector("#hamburger");
 const nav = document.querySelector("#nav");
+const icon = document.querySelector("#icon");
 
 hamburger.addEventListener('click', function() {
   nav.classList.toggle('d-none');
+  if (icon.classList.contains('fa-bars')) {
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-xmark'); // Anciennement fa-times
+  } else {
+    icon.classList.remove('fa-xmark');
+    icon.classList.add('fa-bars');
+  }
 });
