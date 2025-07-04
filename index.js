bce367b89e72c408606df2a3337a5c918a69dd2e
Sary <sary.zeaiter@laplateforme.io>
2025-07-04 11:36:34 +0200

    testing

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-import mock from '../js/mock';
+import mock from "./js/mock";
 
-window.onclick = e => {
-    let titre = e.target.innerText;
-    console.log(titre);
-    return titre;
-}
+// const document = document.querySelector("main");
 
-let filterPerso = [];
+console.log(document.getElementById("test").innerText);
 
-for (let i = 0; i<mock.length; i++){
-    if (mock[i].name === titre){
-        filterPerso = [mock[i]];
-    }
-}
+document.addEventListener( "click", function (e) {
+    let target = this.documentElement;
+    console.log(target.innerText);
+  },
+  false
+);
 
-console.log(filterPerso);
+// window.onclick = (e) => {
+//   let titre = e.target.innerText;
+//   console.log(titre);
+//   return titre;
+// };
 
+// let filterPerso = [];
 
+// for (let i = 0; i < mock.length; i++) {
+//   if (mock[i].name === titre) {
+//     filterPerso = [mock[i]];
+//   }
+// }
 
-
-
+// console.log(filterPerso);
 
 // document.addEventListener('click', function(e) {
 //     e = e || window.e;
 //     let target = e.target || e.srcElement,
-//         text = target.textContent || target.innerText;   
+//         text = target.textContent || target.innerText;
 // }, false);
--- js/mock.js
+++ js/mock.js
@@ -33,3 +33,4 @@ const mock = [
   },
 ];
 
+export default mock;
