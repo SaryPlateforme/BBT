ccfba8a928119767e38255063a4f2d815d8c1db8
Sary <sary.zeaiter@laplateforme.io>
2025-07-04 09:37:31 +0200

    add mock.js

---
+++ js/mock.js
@@ -0,0 +1,23 @@
+const mock = [
+    {
+        name: "Sary",
+        text: "lorem" 
+    },
+    {
+        name: "Thibault",
+        text: "lorem"
+    },
+    {
+        name: "Vitor",
+        text: "lorem"
+    },
+    {
+        name: "Kenzo",
+        text: "lorem"
+    },
+    {
+        name: "Mathieu",
+        text: "lorem"
+    }
+]
+
